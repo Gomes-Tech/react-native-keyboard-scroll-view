7ed01ba5073ee6f6f7d29e46e948bf58a065d377
Marcos Gomes <marcosgomesdev@gmail.com>
2024-10-11 08:10:02 -0300

    feat: update CI commands

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const { getConfig } = require('react-native-builder-bob/babel-config');
-const pkg = require('../package.json');
+import path from 'path';
+import { getConfig } from 'react-native-builder-bob/babel-config';
+import pkg from '../package.json';
 
 const root = path.resolve(__dirname, '..');
 
-module.exports = function (api) {
+export default function (api) {
   api.cache(true);
 
   return getConfig(
@@ -13,4 +13,4 @@ module.exports = function (api) {
     },
     { root, pkg }
   );
-};
+}
